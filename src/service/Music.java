27815d226a412b9198f5cba1de26f98a9d3c4d03
shervin <shervinrajabi2016@gmail.com>
2025-04-18 14:21:44 +0330

    add constructor.

--- src/service/Music.java
+++ src/service/Music.java
@@ -5,20 +5,33 @@ import java.util.ArrayList;
 public class Music {
     private String title;
     private User singer;
-    private int numberOfStreams;
+    private int numberOfStreams = 0;
     private static ArrayList<Music> allMusics = new ArrayList<Music>();
 
+    public Music(String title, User singer) throws InvalidOperationException {
+        if (title.isEmpty() || singer == null) {
+            throw new InvalidOperationException("title and singer cannot be empty");
+        }
+        this.title = title;
+        this.singer = singer;
+    }
+
     public void play() {
-        System.out.println("Now playing: " + title + " by " + singer.getUsername());
+        System.out.println("Now playing: " + title + " by " + singer.getUsername() + "\tstreams : " + numberOfStreams);
         numberOfStreams++;
     }
 
     public static ArrayList<Music> Search (String title) {
+        boolean found = false;
         ArrayList<Music> resault = new ArrayList<Music>();
         for (Music m : allMusics) {
             if (m.title.equals(title)) {
+                found = true;
                 resault.add(m);
             }
+        }
+        if (!found) {
+            return null;
         }
         return resault;
     }
